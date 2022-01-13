b4f999824c6a14e0631e4a97f534f5c00b8d6ffe
Brad Kuhne <bjkuhne@aol.com>
2022-01-12 23:33:00 -0600

    basic inquirer input prompt

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require("fs");
+// const generatePage = require('./src/page-template.js');
 
-//Code from Module 1 below
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// const pageHTML = generatePage(uname, github);
 
-//   console.log('================');
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if (err) throw err;
 
-// printProfileData(profileDataArgs);
-//End of Module 9.1 Code
-// Start of Lesson 9.2 below
-const profileDataArgs = process.argv.slice(2);
-const [uname, github] = profileDataArgs;
-
-fs.writeFile("./index.html", generatePage(uname, github), err => {
-    if (err) throw new Error(err);
-
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
